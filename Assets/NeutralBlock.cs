94de4ca4ded99b8343312a50323c9add6a021f56
hollowaymj <michaeljholloway@gmail.com>
2020-03-26 15:25:30 -0400

    User can create towers on neutral blocks

---
+++ Assets/NeutralBlock.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeutralBlock : MonoBehaviour
+{
+    [SerializeField] GameObject tower;
+    [SerializeField] bool hasTower = false;
+
+    TowersEmpty parent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        parent = FindObjectOfType<TowersEmpty>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!hasTower)
+            {
+                CreateTower();
+            }
+        }
+    }
+
+    private void CreateTower()
+    {
+        Vector3 towerPos = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 10f, gameObject.transform.position.z);
+        GameObject newTower = Instantiate(tower, towerPos, Quaternion.identity);
+        newTower.transform.parent = parent.transform;
+        hasTower = true;
+    }
+}
