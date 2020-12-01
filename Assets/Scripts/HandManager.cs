76876ad68d57148009eafc23dcb21b4ee3db7087
hollowaymj <michaeljholloway@gmail.com>
2020-12-01 14:59:57 -0500

    Fixed the vertical displacement of cards.

--- Assets/Scripts/HandManager.cs
+++ Assets/Scripts/HandManager.cs
@@ -7,14 +7,16 @@ using UnityEngine.UI;
 
 public class HandManager : MonoBehaviour
 {
+    // TODO: Make it deal 5 cards at start, add cards to the hand object and eventually remove the hand list.
     [SerializeField] int handSize = 5;
     [SerializeField] RawImage card;
     [SerializeField] GameObject deckObject;
     [SerializeField] float cardSpacing = 90f;
     [SerializeField] RectTransform lookAt;
+    [SerializeField] float verticalDisplacementMultiplier = 15f;
+    [SerializeField] RectTransform handObject;
     
     List<RawImage> deck = new List<RawImage>();
-    List<RawImage> cards = new List<RawImage>();
     List<RectTransform> hand = new List<RectTransform>();
     int cardsInHand = 0;
     float cardWidth = 260f;
@@ -45,19 +47,62 @@ public class HandManager : MonoBehaviour
             for (int cardNum = 0; cardNum < currentHandSize; cardNum++)
             {
                 SetPosition(currentHandSize, handWidth, cardNum);
-                //float displacement;
-                //if (currentHandSize % 2 == 0)
-                //{
-                //    if ( cardNum < currentHandSize / 2)
-                //    {
-
-                //    }
-                //}
+                SetDisplacement(currentHandSize, cardNum);
                 SetRotation(currentHandSize, cardNum);
             }
             yield return new WaitForSeconds(.25f);
         }
 
+        cardsInHand = currentHandSize;
+
+    }
+
+    // Okay, so here I'm trying to set the vertical displacement of the cards so that they will be farther down the farther they are from the center of the hand.
+    private void SetDisplacement(int currentHandSize, int cardNum)
+    {
+        float displacementFactor;
+        int split = currentHandSize / 2;
+        int switchFactor;
+        // if the hand size is even...
+        if (currentHandSize % 2 == 0)
+        {
+            if (cardNum < currentHandSize / 2)
+            {
+                switchFactor = split - 1 - cardNum; // There are 2 splits for an even hand since there is no true center card. Defining different switchfactors
+            }                                       // also allows me to use one switch function for everything regardless of the contents of the hand.
+                                                    // For example: 10 cards: half is 5, split at 4 & 5 (since card count starts at 0). 0,1,2,3,4 -- 5,6,7,8,9
+            else
+            {
+                switchFactor = cardNum - split;
+            }
+            
+        }
+        // if the hand size is odd...
+        else
+        {
+            switchFactor = Mathf.Abs(split - cardNum);
+        }
+        // It is worth pointing out that these displacement factors were determined purely by experimenting with moving the cards around on the screen and 
+        // seeing what looks right. They might need to be changed if I change literally anything else about how the cards appear on screen.
+        switch (switchFactor)
+        {
+            case 0:
+                displacementFactor = 0;
+                break;
+            case 1:
+                displacementFactor = 1;
+                break;
+            case 4:
+                displacementFactor = 10;
+                break;
+            default:
+                displacementFactor = (switchFactor - 1) * 3;
+                break;
+        }
+        // Just for reminder, the multiplier is what I can change from the inspector to get instant changes. The factor is essentially the ratio of card 
+        // displacement calculated in the previous step.
+        float displacement = verticalDisplacementMultiplier * displacementFactor;
+        hand[cardNum].anchoredPosition = new Vector2(hand[cardNum].anchoredPosition.x, -displacement);
     }
 
     // Calculates the width of the hand based on how many cards are in the hand and the width of the cards, but subtracts the overlap of the cards.
